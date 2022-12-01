b0486600164e3b3dbfcdc4649bf334a2bf025798
HenryAzogue <hnrazogue@gmail.com>
2022-12-01 18:39:39 -0500

    Se agrega el componente footer

---
+++ src/components/FooterPage.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+
+const FooterPage = () => {
+  return (
+    <footer className='section__footer'>
+      <header className='footer__header'>
+        <p className='header__title'>
+          @HenryAzogue
+        </p>
+      </header>
+      {/* <div className="footer__rrss">
+        <ul>
+          <li>
+            <a href="https://github.com/HenryAzogue"
+              referrerPolicy='no-referrer'
+              target="__blank"
+            >
+              GuidHut
+            </a>
+          </li>
+
+          <li>
+            <a href="https://www.instagram.com/henryazogue/"
+              referrerPolicy='no-referrer'
+              target="__blank"
+            >
+              Linkedin
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://www.instagram.com/henryazogue/"
+              referrerPolicy='no-referrer'
+              target="__blank"
+            >Instagram</a>
+          </li>
+
+        </ul>
+      </div> */}
+    </footer>
+  );
+};
+
+export default FooterPage;
